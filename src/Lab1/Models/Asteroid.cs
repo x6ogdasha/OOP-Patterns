7e586a22a797c90e2d30f836d41a114351f81389
Bogdan <6ogdashaa@gmail.com>
2023-09-20 16:15:04 +0300

    feat: added 4 classes of obstacles

---
+++ src/Lab1/Models/Asteroid.cs
@@ -0,0 +1,11 @@
+using Itmo.ObjectOrientedProgramming.Lab1.Entities;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Models;
+
+public class Asteroid : BaseObstacle
+{
+    public Asteroid()
+    {
+        Damage = 5;
+    }
+}
---
+++ src/Lab1/Models/Meteorit.cs
@@ -0,0 +1,11 @@
+using Itmo.ObjectOrientedProgramming.Lab1.Entities;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Models;
+
+public class Meteorit : BaseObstacle
+{
+    public Meteorit()
+    {
+        Damage = 10;
+    }
+}
