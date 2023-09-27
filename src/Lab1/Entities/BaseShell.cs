11ebed20b6e6a08c5b21bfefc1a2538f0c8201a1
Bogdan <6ogdashaa@gmail.com>
2023-09-27 21:37:29 +0300

    refactor: described method in BaseShell which takes remaining damage from deflector

--- src/Lab1/Entities/BaseShell.cs
+++ src/Lab1/Entities/BaseShell.cs
@@ -2,4 +2,12 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Entities;
 
 public abstract class BaseShell : Protection
 {
+    public void TakeRemainDamage(double damage)
+    {
+        HealthPoints -= damage;
+        if (HealthPoints <= 0)
+        {
+            IsActive = false;
+        }
+    }
 }
