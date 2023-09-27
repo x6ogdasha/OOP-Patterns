67266d54296c297968414b11c38d4526f13cb416
Bogdan <6ogdashaa@gmail.com>
2023-09-27 21:38:49 +0300

    refactor: updated BaseShip TakeDamage method

--- src/Lab1/Entities/BaseShip.cs
+++ src/Lab1/Entities/BaseShip.cs
@@ -1,6 +1,7 @@
 using System;
 using Itmo.ObjectOrientedProgramming.Lab1.Helpers;
 using Itmo.ObjectOrientedProgramming.Lab1.Models.Engines;
+using Itmo.ObjectOrientedProgramming.Lab1.Models.Obstacles;
 using Itmo.ObjectOrientedProgramming.Lab1.Models.Shells;
 
 namespace Itmo.ObjectOrientedProgramming.Lab1.Entities;
@@ -12,14 +13,32 @@ public abstract class BaseShip
     public BaseDeflector? Deflector { get; protected set; }
     public ShipSize Size { get; protected set; }
     public BaseShell Shell { get; protected set; } = new ShellRank1();
-    public bool IsShipIntact { get; protected set; }
-    public bool IsCrewAlive { get; protected set; }
-    public bool AntiNitrineRadiant { get; protected set; }
+    public bool? IsShipIntact { get; protected set; }
+    public bool? IsCrewAlive { get; protected set; }
+    public bool? AntiNitrineRadiant { get; protected set; }
+    public string Status => $"Deflector HP: {Deflector?.HealthPoints}, Shell HP: {Shell.HealthPoints}";
 
     public void TakeDamage(BaseObstacle obstacle, int numberOfObstacles)
     {
-        Deflector?.TakeDamage(obstacle, numberOfObstacles);
-        Shell.TakeDamage(obstacle, numberOfObstacles);
+        if (obstacle == null) throw new ArgumentNullException(nameof(obstacle));
+        if (AntiNitrineRadiant == true && obstacle.GetType() == typeof(SpaceWhale))
+        {
+            return;
+        }
+
+        if (Deflector?.IsActive == true)
+        {
+            Deflector.TakeDamage(obstacle, numberOfObstacles);
+            if (Deflector.RemainingDamage != 0)
+            {
+                Shell.TakeRemainDamage(Deflector.RemainingDamage);
+            }
+        }
+        else
+        {
+            Shell.TakeDamage(obstacle, numberOfObstacles);
+        }
+
         if (Shell.IsActive == false)
         {
             IsShipIntact = false;
