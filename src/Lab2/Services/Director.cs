edd2e5b6fd0d0ef0d5b306a5be9b70369915da47
Bogdan <6ogdashaa@gmail.com>
2023-10-24 20:06:13 +0300

    refactor: Client class became Director and implements building of computer

---
+++ src/Lab2/Services/Director.cs
@@ -0,0 +1,33 @@
+using System;
+using Itmo.ObjectOrientedProgramming.Lab2.Entities.Builder;
+using Itmo.ObjectOrientedProgramming.Lab2.Entities.Repositories;
+
+namespace Itmo.ObjectOrientedProgramming.Lab2.Services;
+
+public class Director
+{
+    private RepositoryContext _currentRepository = new RepositoryContext();
+    private CreateComponents _service;
+    private ComputerBuilder _builder;
+    public Director()
+    {
+        _builder = new ComputerBuilder();
+        _service = new CreateComponents(_currentRepository);
+    }
+
+    public void BuildComputer(string motherName, string cpuName, string? biosName, string coolingName, string ramName, string? xmpName, string? gpuName, string? ssdName, string? hddName, string caseName, string powerName, string? wifiName)
+    {
+        _builder.MotherBoard(_service.Repository.MotherRepository.Read(motherName) ?? throw new InvalidOperationException());
+        _builder.CPU(_service.Repository.CpuRepository.Read(cpuName) ?? throw new InvalidOperationException());
+        _builder.BIOS(_service.Repository.BiosRepository.Read(biosName));
+        _builder.CoolingSystem(_service.Repository.CoolingRepository.Read(coolingName) ?? throw new InvalidOperationException());
+        _builder.RAM(_service.Repository.RamRepository.Read(ramName) ?? throw new InvalidOperationException());
+        _builder.XMP(_service.Repository.XmpRepository.Read(xmpName));
+        _builder.GPU(_service.Repository.GpuRepository.Read(gpuName));
+        _builder.SSD(_service.Repository.SsdRepository.Read(ssdName));
+        _builder.HDD(_service.Repository.HddRepository.Read(hddName));
+        _builder.ComputerCase(_service.Repository.CaseRepository.Read(caseName) ?? throw new InvalidOperationException());
+        _builder.PowerBlock(_service.Repository.PowerRepository.Read(powerName) ?? throw new InvalidOperationException());
+        _builder.WiFiAdapter(_service.Repository.WifiRepository.Read(wifiName));
+    }
+}
